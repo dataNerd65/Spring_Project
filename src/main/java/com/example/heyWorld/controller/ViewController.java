1654a17296022cb88520d906695cb2021e75cee8
dataNerd65 <pkiarie811@gmail.com>
2025-11-18 22:05:33 +0300

    controller of the site

--- src/main/java/com/example/heyWorld/controller/ViewController.java
+++ src/main/java/com/example/heyWorld/controller/ViewController.java
@@ -1,4 +1,21 @@
 package com.example.heyWorld.controller;
+import com.example.heyWorld.model.Task;
+import com.example.heyWorld.repository.TaskRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class ViewController {
+    private final TaskRepository taskRepository;
+
+    public ViewController(TaskRepository taskRepository){
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping("/tasks-view")
+    public String getTasksPage(Model model){
+        model.addAttribute("tasks", taskRepository.findAll());
+        return "tasks"; // maps to tasks.html
+    }
 }
